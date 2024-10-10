1bfa37068b9749a9923c0a3fe402ac5f0354cfc9
Drew Taylor <drew@chainofinsight.com>
2024-10-10 13:45:42 -0400

    Added chat log export

--- chat.py
+++ chat.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 
 import os
+import time
+import json
 import asyncio
 from dotenv import load_dotenv
 from openai import AsyncOpenAI
@@ -13,7 +15,9 @@ client = AsyncOpenAI(
 
 model = os.environ.get('OPENAI_MODEL')
 
-suffix = " Your response should be silly and sassy."
+data_dir = './log/'
+log_suffix = '.json'
+chat_history = []
 
 class bcolors:
     HEADER = '\033[95m'
@@ -29,31 +33,54 @@ class bcolors:
 # Chat worker
 async def ask(prompt) -> None:
     print("\n")
-    print(bcolors.OKCYAN + "gpt-4o-mini" + bcolors.ENDC + ":\n")
+    print(bcolors.OKCYAN + model + " says:" + bcolors.ENDC + "\n")
+
+    user_message = {
+        "role": "user",
+        "content": prompt,
+    }
 
     stream = await client.chat.completions.create(
-        messages=[
-            {
-                "role": "user",
-                "content": prompt + suffix,
-            }
-        ],
+        messages=[user_message],
         model=model,
         stream=True,
     )
 
+    chunks = []
     async for chunk in stream:
+        chunks.append(chunk.choices[0].delta.content or "")
         print(chunk.choices[0].delta.content or "", end="")
     
+    assembled_chunks = "".join(chunks)
+    assistant_message = {
+        "role": "assistant",
+        "content": assembled_chunks,
+    }
+
+    chat_history.append(user_message)
+    chat_history.append(assistant_message)
+
     # End of content
     print("\n")
 
+# Export log
+def export():
+    timestamp = int(time.time())
+    file = data_dir + str(timestamp) + log_suffix
+    
+    with open(file, 'w') as f:
+        json.dump(chat_history, f)
+        print(bcolors.OKGREEN + "Chat history exported to " + file)
+
 # Dynamic chat instance
 async def chat():
-    print(bcolors.OKCYAN + "What would you like to know?" + bcolors.ENDC + "\n" + bcolors.OKBLUE + "('x' to exit)" + bcolors.ENDC + "\n")
+    print(bcolors.OKCYAN + "What would you like to know?" + bcolors.ENDC + "\n" + bcolors.OKBLUE + "('x' to exit, 'log' to export a chat log)" + bcolors.ENDC + "\n")
 
     prompt = input()
-    if prompt != 'x':
+
+    if prompt == 'log':
+        export()
+    elif prompt != 'x':
         await ask(prompt)
         await chat()
 
